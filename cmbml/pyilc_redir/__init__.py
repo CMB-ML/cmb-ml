24918dd04b09441be260f87eba72dc142eef661f
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-11-02 17:06:44 -0500

    hardcoded access for lab server

--- cmbml/pyilc_redir/__init__.py
+++ cmbml/pyilc_redir/__init__.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('/home/jim/VSCode_Projects_SPACE/no_mods/pyilc/pyilc')
 sys.path.append('/shared/code/pyilc/pyilc')
+sys.path.append('/bigdata/cmb_project/code/pyilc/pyilc')
 
 from input import ILCInfo
 from wavelets import Wavelets, wavelet_ILC, harmonic_ILC
