cbf9c0fc15f7c31ec59ffab79ea04bd07b3f9caf
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-11-03 20:31:55 -0600

    Removed junk code; tidied

--- cmbml/sims/physics_instrument/physics_instrument_noise_variance.py
+++ cmbml/sims/physics_instrument/physics_instrument_noise_variance.py
@@ -1,6 +1,9 @@
 import logging
 
-from cmbml.sims.physics_instrument.physics_scale_cache_maker import ScaleCacheMaker, make_random_noise_map
+from cmbml.sims.physics_instrument.physics_scale_cache_maker import (
+    ScaleCacheMaker, 
+    make_random_noise_map
+    )
 from cmbml.core.config_helper import ConfigHelper
 
 
@@ -10,21 +13,14 @@ logger = logging.getLogger(__name__)
 class VarianceNoise:
     do_cache = True
     cache_maker = ScaleCacheMaker
-    # This class is used to generate noise maps from Planck's observation maps.
-    # It is used in the NoiseCacheExecutor and SimCreatorExecutor classes.
-    # This class is glue code. The functions afterwards are relevant to Physics.
-    def __init__(self, cfg, name_tracker, scale_cache, in_varmap_src=None):
+    # This class generates noise maps from Planck's observation maps.
+    #    It is used in the NoiseCacheExecutor and SimCreatorExecutor classes.
+    #    This class is glue code. The functions relevant to Physics are in 
+    #    physics_scale_cache_maker.py.
+    def __init__(self, cfg, name_tracker, scale_cache):
         self.nside_out = cfg.scenario.nside
         self.name_tracker = name_tracker
         self.in_scale_cache = scale_cache
-
-        # Use a ConfigHelper to get the assets in; we don't need them as arguments.
-        _ch = ConfigHelper(cfg, 'make_noise_cache')  # Applicable to both
-                                                     #   NoiseCache and SimCreator
-        assets_in = _ch.get_assets_in(name_tracker=self.name_tracker)
-        self.in_varmap_src = assets_in["noise_src_varmaps"]
-
-        self.in_varmap_src = in_varmap_src
 
     def get_noise_map(self, freq, noise_seed):
         """
@@ -40,4 +36,3 @@ class VarianceNoise:
             sd_map = self.in_scale_cache.read()
             noise_map = make_random_noise_map(sd_map, noise_seed)
             return noise_map
-
