9111da85e02ee152651435d96952e5f0bfa5b6d2
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-11-07 17:08:12 -0600

    updated path

--- rename_obs.py
+++ rename_obs.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 
-datasets_root   = '/shared/data/Datasets/I_512_1450_v2/'
-dataset_name    = 'I_512_1450_v2'
+datasets_root   = '/shared/data/Datasets/'
+dataset_name    = 'I_512_1450_Separate_Noise'
 working_dir_in  = 'Simulation_Working/Simulation_D_No_Noise'
 working_dir_out = 'Simulation'
 
@@ -10,11 +10,11 @@ splits = {'Test':200,
           'Train':1000, 
           'Validation':250}
 
-detectors = [30, 44, 70, 100, 143, 217, 353, 545, 857]
+# detectors = [30, 44, 70, 100, 143, 217, 353, 545, 857]
 
 
-in_fn_template = 'obs_{det}_map.fits'
-out_fn_template = 'sky_{det}_no_noise_map.fits'
+# in_fn_template = 'obs_{det}_map.fits'
+# out_fn_template = 'sky_{det}_no_noise_map.fits'
 
 in_cmb_template = 'cmb_map_fid.fits'
 out_cmb_template = 'cmb_map_fid.fits'
@@ -22,11 +22,11 @@ out_cmb_template = 'cmb_map_fid.fits'
 for split in splits:
     for sim_num in range(splits[split]):
         in_dir = Path(datasets_root) / dataset_name / working_dir_in / split / f'sim{sim_num:04d}'
-        for det in detectors:
-            old_fp = in_dir / in_fn_template.format(det=det)
-            if not old_fp.exists():
-                print(f"{old_fp} does not exist! Aborting.")
-                exit()
+        # for det in detectors:
+        #     old_fp = in_dir / in_fn_template.format(det=det)
+        #     if not old_fp.exists():
+        #         print(f"{old_fp} does not exist! Aborting.")
+        #         exit()
         old_fp = in_dir / in_cmb_template
         if not old_fp.exists():
             print(f"{old_fp} does not exist! Aborting.")
@@ -37,12 +37,12 @@ for split in splits:
     for sim_num in range(splits[split]):
         in_dir = Path(datasets_root) / dataset_name / working_dir_in / split / f'sim{sim_num:04d}'
         out_dir = Path(datasets_root) / dataset_name / working_dir_out / split / f'sim{sim_num:04d}'
-        for det in detectors:
-            old_fp = in_dir / in_fn_template.format(det=det)
-            new_fp = out_dir / out_fn_template.format(det=det)
-            new_fp.parent.mkdir(parents=True, exist_ok=True)
-            print(f"Renaming {old_fp} to {new_fp}")
-            old_fp.rename(new_fp)
+        # for det in detectors:
+        #     old_fp = in_dir / in_fn_template.format(det=det)
+        #     new_fp = out_dir / out_fn_template.format(det=det)
+        #     new_fp.parent.mkdir(parents=True, exist_ok=True)
+        #     print(f"Renaming {old_fp} to {new_fp}")
+        #     old_fp.rename(new_fp)
         old_fp = in_dir / in_cmb_template
         new_fp = out_dir / out_cmb_template
         new_fp.parent.mkdir(parents=True, exist_ok=True)
