ab3c7acd853121a8c2f20557e943a027e2beaa82
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-11-09 21:25:45 -0600

    changed pipeline yamls for cmbnncs executors

--- main_cmbnncs.py
+++ main_cmbnncs.py
@@ -66,7 +66,8 @@ from cmbml.analysis import (ShowSimsPrepExecutor,
                             PowerSpectrumAnalysisExecutorSerial,
                             PowerSpectrumSummaryExecutor,
                             PowerSpectrumSummaryFigsExecutor,
-                            PostAnalysisPsFigExecutor)
+                            PostAnalysisPsFigExecutor,
+                            ShowOnePSExecutor)
 
 
 logger = logging.getLogger(__name__)
@@ -84,43 +85,44 @@ def run_cmbnncs(cfg):
     pipeline_context.add_pipe(HydraConfigCheckerExecutor)
     pipeline_context.add_pipe(HydraConfigCMBNNCSCheckerExecutor)
 
-    pipeline_context.add_pipe(PreprocessMakeScaleExecutor)
-    # pipeline_context.add_pipe(NonParallelPreprocessExecutor)  # For demonstration only
-    pipeline_context.add_pipe(PreprocessExecutor)
-    pipeline_context.add_pipe(ShowSimsPrepExecutor)
+    # pipeline_context.add_pipe(PreprocessMakeScaleExecutor)
+    # # pipeline_context.add_pipe(NonParallelPreprocessExecutor)  # For demonstration only
+    # pipeline_context.add_pipe(PreprocessExecutor)
+    # pipeline_context.add_pipe(ShowSimsPrepExecutor)
 
-    pipeline_context.add_pipe(TrainingExecutor)
+    # pipeline_context.add_pipe(TrainingExecutor)
 
-    pipeline_context.add_pipe(PredictionExecutor)
-    pipeline_context.add_pipe(CMBNNCSShowSimsPredExecutor)
-    pipeline_context.add_pipe(PostprocessExecutor)
-    # pipeline_context.add_pipe(NonParallelPostprocessExecutor)  # For demonstration only
+    # pipeline_context.add_pipe(PredictionExecutor)
+    # pipeline_context.add_pipe(CMBNNCSShowSimsPredExecutor)
+    # pipeline_context.add_pipe(PostprocessExecutor)
+    # # pipeline_context.add_pipe(NonParallelPostprocessExecutor)  # For demonstration only
 
-    pipeline_context.add_pipe(MaskCreatorExecutor)
+    # pipeline_context.add_pipe(MaskCreatorExecutor)
 
-    # In the following, "Common" means "Apply the same postprocessing to all models"; requires a mask
-    # Apply to the target (CMB realization)
-    pipeline_context.add_pipe(CommonRealPostExecutor)
-    # Apply to CMBNNCS's predictions
-    pipeline_context.add_pipe(CommonCMBNNCSPredPostExecutor)
+    # # In the following, "Common" means "Apply the same postprocessing to all models"; requires a mask
+    # # Apply to the target (CMB realization)
+    # pipeline_context.add_pipe(CommonRealPostExecutor)
+    # # Apply to CMBNNCS's predictions
+    # pipeline_context.add_pipe(CommonCMBNNCSPredPostExecutor)
 
-    # Show results of cleaning
-    pipeline_context.add_pipe(CommonCMBNNCSShowSimsPostExecutor)
+    # # Show results of cleaning
+    # pipeline_context.add_pipe(CommonCMBNNCSShowSimsPostExecutor)
 
-    pipeline_context.add_pipe(PixelAnalysisExecutor)
-    pipeline_context.add_pipe(PixelSummaryExecutor)
-    pipeline_context.add_pipe(PixelSummaryFigsExecutor)
+    # pipeline_context.add_pipe(PixelAnalysisExecutor)
+    # pipeline_context.add_pipe(PixelSummaryExecutor)
+    # pipeline_context.add_pipe(PixelSummaryFigsExecutor)
 
-    # These two do not need to run individually for all models
-    pipeline_context.add_pipe(ConvertTheoryPowerSpectrumExecutor)
-    pipeline_context.add_pipe(MakeTheoryPSStats)
+    # # These two do not need to run individually for all models
+    # pipeline_context.add_pipe(ConvertTheoryPowerSpectrumExecutor)
+    # pipeline_context.add_pipe(MakeTheoryPSStats)
 
-    # # CMBNNCS's Predictions as Power Spectra Anaylsis
+    # # # CMBNNCS's Predictions as Power Spectra Anaylsis
     pipeline_context.add_pipe(CMBNNCSMakePSExecutor)
-    pipeline_context.add_pipe(PSAnalysisExecutor)
-    pipeline_context.add_pipe(PowerSpectrumSummaryExecutor)
-    pipeline_context.add_pipe(PowerSpectrumSummaryFigsExecutor)
-    pipeline_context.add_pipe(PostAnalysisPsFigExecutor)
+    pipeline_context.add_pipe(ShowOnePSExecutor)
+    # pipeline_context.add_pipe(PSAnalysisExecutor)
+    # pipeline_context.add_pipe(PowerSpectrumSummaryExecutor)
+    # pipeline_context.add_pipe(PowerSpectrumSummaryFigsExecutor)
+    # pipeline_context.add_pipe(PostAnalysisPsFigExecutor)
 
     pipeline_context.prerun_pipeline()
 
