c6bd839d32686fdd215574bb7d2a81ec3c5d93c9
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-11-18 12:04:33 -0600

    added run_inference_on switch

--- main_cmbnncs.py
+++ main_cmbnncs.py
@@ -54,6 +54,7 @@ from cmbml.analysis import (ShowSimsPrepExecutor,
                             CommonRealPostExecutor,
                             CommonCMBNNCSPredPostExecutor,
                             CommonCMBNNCSShowSimsPostExecutor,
+                            CommonCMBNNCSShowSimsPostIndivExecutor,
                             CMBNNCSShowSimsPredExecutor, 
                             # CMBNNCSShowSimsPostExecutor,
                             PixelAnalysisExecutor,
@@ -100,28 +101,29 @@ def run_cmbnncs(cfg):
 
     # In the following, "Common" means "Apply the same postprocessing to all models"; requires a mask
     # Apply to the target (CMB realization)
-    pipeline_context.add_pipe(CommonRealPostExecutor)
+    # pipeline_context.add_pipe(CommonRealPostExecutor)
     # Apply to CMBNNCS's predictions
-    pipeline_context.add_pipe(CommonCMBNNCSPredPostExecutor)
+    # pipeline_context.add_pipe(CommonCMBNNCSPredPostExecutor)
 
     # Show results of cleaning
-    pipeline_context.add_pipe(CommonCMBNNCSShowSimsPostExecutor)
+    # pipeline_context.add_pipe(CommonCMBNNCSShowSimsPostExecutor)
+    pipeline_context.add_pipe(CommonCMBNNCSShowSimsPostIndivExecutor)
 
-    pipeline_context.add_pipe(PixelAnalysisExecutor)
-    pipeline_context.add_pipe(PixelSummaryExecutor)
-    pipeline_context.add_pipe(PixelSummaryFigsExecutor)
+    # pipeline_context.add_pipe(PixelAnalysisExecutor)
+    # pipeline_context.add_pipe(PixelSummaryExecutor)
+    # pipeline_context.add_pipe(PixelSummaryFigsExecutor)
 
-    # These two do not need to run individually for all models (but they're fast, so it doesn't matter unless you're actively changing them)
-    pipeline_context.add_pipe(ConvertTheoryPowerSpectrumExecutor)
-    pipeline_context.add_pipe(MakeTheoryPSStats)
+    # # These two do not need to run individually for all models (but they're fast, so it doesn't matter unless you're actively changing them)
+    # pipeline_context.add_pipe(ConvertTheoryPowerSpectrumExecutor)
+    # pipeline_context.add_pipe(MakeTheoryPSStats)
 
-    # # # CMBNNCS's Predictions as Power Spectra Anaylsis
-    pipeline_context.add_pipe(CMBNNCSMakePSExecutor)
-    pipeline_context.add_pipe(PSAnalysisExecutor)
-    pipeline_context.add_pipe(PowerSpectrumSummaryExecutor)
-    pipeline_context.add_pipe(PowerSpectrumSummaryFigsExecutor)
-    pipeline_context.add_pipe(PostAnalysisPsFigExecutor)
-    # pipeline_context.add_pipe(ShowOnePSExecutor)  # Used for debugging; does not require full set of theory ps for simulations
+    # # # # CMBNNCS's Predictions as Power Spectra Anaylsis
+    # pipeline_context.add_pipe(CMBNNCSMakePSExecutor)
+    # pipeline_context.add_pipe(PSAnalysisExecutor)
+    # pipeline_context.add_pipe(PowerSpectrumSummaryExecutor)
+    # pipeline_context.add_pipe(PowerSpectrumSummaryFigsExecutor)
+    # pipeline_context.add_pipe(PostAnalysisPsFigExecutor)
+    # # pipeline_context.add_pipe(ShowOnePSExecutor)  # Used for debugging; does not require full set of theory ps for simulations
 
     pipeline_context.prerun_pipeline()
 
