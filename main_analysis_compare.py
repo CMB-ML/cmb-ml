cdf900cca80a8a163116667654dbf4be23dac3a2
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-12-13 10:26:01 -0600

    enabled all stages of analysis comparison

--- main_analysis_compare.py
+++ main_analysis_compare.py
@@ -26,10 +26,10 @@ def run_pyilc_analysis(cfg):
 
     pipeline_context = PipelineContext(cfg, log_maker)
 
-    # pipeline_context.add_pipe(HydraConfigCheckerExecutor)
+    pipeline_context.add_pipe(HydraConfigCheckerExecutor)
 
-    # pipeline_context.add_pipe(PixelCompareTableExecutor)
-    # pipeline_context.add_pipe(PSCompareTableExecutor)
+    pipeline_context.add_pipe(PixelCompareTableExecutor)
+    pipeline_context.add_pipe(PSCompareTableExecutor)
     pipeline_context.add_pipe(PostAnalysisPsCompareFigExecutor)
 
     pipeline_context.prerun_pipeline()
