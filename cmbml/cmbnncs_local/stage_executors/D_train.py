163ea432dc79f09c35eba665c3fb0cdeeebf4d08
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-12-16 21:13:51 -0600

    cleanup

--- cmbml/cmbnncs_local/stage_executors/D_train.py
+++ cmbml/cmbnncs_local/stage_executors/D_train.py
@@ -36,7 +36,7 @@ class TrainingExecutor(BaseCMBNNCSModelExecutor):
         self.in_model: Asset = self.assets_in["model"]
         self.in_cmb_asset: Asset = self.assets_in["cmb_map"]
         self.in_obs_assets: Asset = self.assets_in["obs_maps"]
-        self.in_norm: Asset = self.assets_in["norm_file"]
+        # self.in_norm: Asset = self.assets_in["norm_file"]  # TODO: Does removing this line break anything?
         in_model_handler: PyTorchModel
         in_cmb_map_handler: NumpyMap
         in_obs_map_handler: NumpyMap
