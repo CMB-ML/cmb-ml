db6ce4f73a8594de7a3d2f95aabfc5e1d7ebe813
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2025-01-05 15:27:21 -0600

    added dataset for inference ("test")

--- cmbml/patch_nn_test/dataset.py
+++ cmbml/patch_nn_test/dataset.py
@@ -34,9 +34,8 @@ class TrainCMBPatchDataset(Dataset):
         self.feature_handler = feature_handler
         self.n_map_fields:int = len(map_fields)
         self.which_patch_dict = which_patch_dict
-        logger.debug("Building patch LUT...")
         self.patch_lut = lut
-        logger.debug(f"Patch LUT built. Shape: {self.patch_lut.shape}")
+        logger.debug(f"Patch LUT shape: {self.patch_lut.shape}")
 
     def __len__(self):
         return self.n_sims
@@ -64,47 +63,69 @@ class TrainCMBPatchDataset(Dataset):
         return features_tensor, label_tensor, sim_idx, patch_id  # For debugging
         # return features_tensor, label  # For regular use
 
-
-class TestCMBMapDataset(Dataset):
+class TestCMBPatchDataset(Dataset):
+    """
+    Creates a dataset, iterating over patches for a single simulation.
+    """
     def __init__(self, 
                  n_sims,
                  freqs,
-                 map_fields,
-                #  label_path_template,
-                #  label_handler,
+                 map_fields: str,
                  feature_path_template,
                  feature_handler,
+                 sim_idx,
+                 lut
                  ):
+        # TODO: Adopt similar method as in parallel operations to allow 
+        #       this to use num_workers and transforms
         self.n_sims = n_sims
         self.freqs = freqs
-        # self.label_path_template = label_path_template
-        # self.label_handler = label_handler
         self.feature_path_template = feature_path_template
         self.feature_handler = feature_handler
         self.n_map_fields:int = len(map_fields)
-        raise NotImplementedError("This class is not implemented yet.")
+        self.patch_lut = lut
+        logger.debug(f"Patch LUT built. Shape: {self.patch_lut.shape}")
 
-    # def __len__(self):
-    #     return self.n_sims
-    
-    # def __getitem__(self, sim_idx):
-    #     # label_path = self.label_path_template.format(sim_idx=sim_idx)
-    #     # label = self.label_handler.read(label_path)
-    #     # label_tensor = torch.as_tensor(label)
+        self._sim_idx = None
+        self._current_map_data = None
 
-    #     features = _get_features_idx(freqs=self.freqs,
-    #                                  path_template=self.feature_path_template,
-    #                                  handler=self.feature_handler,
-    #                                  n_map_fields=self.n_map_fields,
-    #                                  sim_idx=sim_idx)
+    def __len__(self):
+        # We use the number of patches defined as the length of the dataset
+        return self.patch_lut.shape[0]
 
+    @property
+    def sim_idx(self):
+        return self._sim_idx
 
-    #     # feature_path_template = self.feature_path_template.format(sim_idx=sim_idx, det="{det}")
-    #     # features = self.feature_handler.read(feature_path_template)
-    #     features_tensor = tuple([torch.as_tensor(f) for f in features])
-    #     features_tensor = torch.cat(features_tensor, dim=0)
-    #     return features_tensor, sim_idx
-    
+    @sim_idx.setter
+    def sim_idx(self, value):
+        """Setter for sim_idx. Reads the map file when the value is set."""
+        if value != self._sim_idx:  # Avoid re-reading if the same value is set
+            self._sim_idx = value
+            features = []
+            for freq in self.freqs:
+                feature_path = self.feature_path_template.format(sim_idx=self.sim_idx, freq=freq)
+                feature_data = self.feature_handler.read(feature_path)
+                # Strip out the other map fields and the astropy units information:
+                feature_data = feature_data[0].value  # TODO: Implement multiple fields
+                features.append(feature_data)
+            self._current_map_data = features
+            print(f"Loaded map data for simulation index {value}")
+
+    @property
+    def current_map_data(self):
+        return self._current_map_data
+
+    def __getitem__(self, patch_id):
+        r_ids = self.patch_lut[patch_id]
+        features = [m[r_ids] for m in self.current_map_data]
+
+        features_tensor = tuple([torch.as_tensor(f) for f in features])
+        features_tensor = torch.stack(features_tensor, dim=0)
+
+        return features_tensor, self.sim_idx, patch_id  # For debugging
+        # return features_tensor, label  # For regular use
+
 
 def _get_features_idx(freqs, path_template, handler, n_map_fields, sim_idx, r_ids):
     # TODO: Implement multiple fields
