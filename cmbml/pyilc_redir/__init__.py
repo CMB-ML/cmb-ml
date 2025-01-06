91ee509da9d7600e2e1ade72936e1b96eb7f4a20
sethrippentrop <sethrippentrop@gmail.com>
2025-01-06 14:03:09 -0600

    Changed configs to run on server

--- cmbml/pyilc_redir/__init__.py
+++ cmbml/pyilc_redir/__init__.py
@@ -1,6 +1,8 @@
 import sys
-sys.path.append('/home/jim/VSCode_Projects_SPACE/no_mods/pyilc/pyilc')
-sys.path.append('/shared/code/pyilc/pyilc')
+#sys.path.append('/home/jim/VSCode_Projects_SPACE/no_mods/pyilc/pyilc')
+#sys.path.append('/shared/code/pyilc/pyilc')
+sys.path.append('/bigdata/seth/pyilc-main/pyilc')
+#sys.path.append('/shared/code/pyilc/pyilc')
 
 from input import ILCInfo
 from wavelets import Wavelets, wavelet_ILC, harmonic_ILC
