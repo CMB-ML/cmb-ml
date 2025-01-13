642e2b9d06c51651d7ddd457e6cd468197ddc4f9
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2025-01-13 16:55:19 -0600

    updated to match actual training code

--- cmbml/patch_nn_test/stage_executors/_E1_train_try_dataloader.py
+++ cmbml/patch_nn_test/stage_executors/_E1_train_try_dataloader.py
@@ -2,7 +2,7 @@ import logging
 
 from tqdm import tqdm
 
-# import multiprocessing as mp
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 # from torch.optim.lr_scheduler import LambdaLR
@@ -12,18 +12,12 @@ import healpy as hp
 
 from cmbml.core import Split, Asset
 from cmbml.core.executor_base import BaseStageExecutor
-from cmbml.core.asset_handlers.asset_handlers_base import Config
-from cmbml.core.asset_handlers.pytorch_model_handler import PyTorchModel # Import for typing hint
-from cmbml.core.asset_handlers.healpy_map_handler import HealpyMap
-from cmbml.core.asset_handlers.handler_npymap import NumpyMap
-# from core.pytorch_dataset import TrainCMBMapDataset
+from cmbml.core.asset_handlers import Config, PyTorchModel, HealpyMap, NumpyMap
 from cmbml.patch_nn_test.dataset import TrainCMBMap2PatchDataset
-# from cmbml.core.pytorch_transform import TrainToTensor
-# from cmbml.cmbnncs_local.preprocessing.scale_methods_factory import get_scale_class
-# from cmbml.cmbnncs_local.preprocessing.transform_pixel_rearrange import sphere2rect
 from cmbml.utils.planck_instrument import make_instrument, Instrument
 from cmbml.patch_nn_test.utils.display_help import show_patch
 from cmbml.patch_nn_test.stage_executors._pytorch_executor_base import BasePyTorchModelExecutor
+from cmbml.patch_nn_test.utils.minmax_scale import MinMaxScaler
 
 
 logger = logging.getLogger(__name__)
@@ -31,26 +25,29 @@ logger = logging.getLogger(__name__)
 
 class TrainingTryDataloaderExecutor(BasePyTorchModelExecutor):
     def __init__(self, cfg: DictConfig) -> None:
-        super().__init__(cfg, stage_str="train")
+        super().__init__(cfg, stage_str="train_no_preprocess")
 
-        self.out_model: Asset = self.assets_out["model"]
-        out_model_handler: PyTorchModel
-
-        self.in_model: Asset = self.assets_in["model"]
         self.in_cmb_asset: Asset = self.assets_in["cmb_map"]
         self.in_obs_assets: Asset = self.assets_in["obs_maps"]
+        self.in_lut_asset: Asset = self.assets_in["lut"]
+        self.in_norm_file: Asset = self.assets_in["norm_file"]
         self.in_all_p_ids_asset: Asset = self.assets_in["patch_dict"]
-        # self.in_norm: Asset = self.assets_in["norm_file"]  # We may need this later
-        in_model_handler: PyTorchModel
+
         in_cmb_map_handler: HealpyMap
         in_obs_map_handler: HealpyMap
-        # in_norm_handler: Config
+        in_norm_handler: Config
+        in_lut_handler: NumpyMap
+        in_all_p_ids_handler: Config
 
         self.nside_patch = cfg.model.patches.nside_patch
 
-        self.choose_device(cfg.model.patch_nn.train.device)
-        self.n_epochs   = cfg.model.patch_nn.train.n_epochs
-        self.batch_size = cfg.model.patch_nn.train.batch_size
+        self.batch_size = 4  # Batch size can be very large; for demo purposes, we use 4
+        # self.batch_size = cfg.model.patch_nn.train.batch_size
+
+        self.scaling = cfg.model.patch_nn.get("scaling", None)
+        if self.scaling and self.scaling != "minmax":
+            msg = f"Only minmax scaling is supported, not {self.scaling}."
+            raise NotImplementedError(msg)
 
     def execute(self) -> None:
         logger.debug(f"Running {self.__class__.__name__} execute()")
@@ -63,25 +60,17 @@ class TrainingTryDataloaderExecutor(BasePyTorchModelExecutor):
             shuffle=False,  # TODO: Change to True (when using the full dataset)
             )
 
-        # model = self.make_model().to(self.device)
-        # loss_function = torch.nn.L1Loss(reduction='mean')
-        # optimizer = torch.optim.Adam(model.parameters(), lr=lr_init)
-        start_epoch = 0  # Temporary TODO: Remove, replace with epoch restart from elsewhere
-
-        for epoch in range(start_epoch, self.n_epochs):
-            batch_n = 0
-            with tqdm(dataloader, postfix={'Loss': 0}) as pbar:
-                for train_features, train_label, sim_idx, p_idx in pbar:
-                    logger.debug(f"train label shape: {train_label.shape}", flush=True)
-                    logger.debug(f"train features shape: {train_features.shape}", flush=True)
-                    for i in range(self.batch_size):
-                        show_patch(train_label[i, 0, :], train_features[i, :], 
-                                   f"Batch {batch_n}, Sample {i}, Train{sim_idx[i]:04d}, Patch {p_idx[i]}")
-                    if sim_idx[-1] >= 10 - self.batch_size:
-                        # I have 10 sims, so this will show the last full batch (and crash after)
-                        break
-                    batch_n += 1
-            break
+        batch_n = 0
+        for train_features, train_label, sim_idx, p_idx in dataloader:
+            logger.debug(f"train label shape: {train_label.shape}", flush=True)
+            logger.debug(f"train features shape: {train_features.shape}", flush=True)
+            for i in range(self.batch_size):
+                show_patch(train_label[i, 0, :], train_features[i, :], 
+                            f"Batch {batch_n}, Sample {i}, Train{sim_idx[i]:04d}, Patch {p_idx[i]}")
+            if sim_idx[-1] >= 10 - self.batch_size:
+                # I have 10 sims, so this will show the last *full* batch
+                break
+            batch_n += 1
 
     def set_up_dataset(self, template_split: Split) -> None:
         cmb_path_template = self.make_fn_template(template_split, self.in_cmb_asset)
@@ -90,19 +79,23 @@ class TrainingTryDataloaderExecutor(BasePyTorchModelExecutor):
         with self.name_tracker.set_context("split", template_split.name):
             which_patch_dict = self.get_patch_dict()
 
+        transform = None
+        if self.scaling == "minmax":
+            vmins = np.array([self.extrema[f]["I"]["vmin"].value for f in self.instrument.dets.keys()])
+            vmaxs = np.array([self.extrema[f]["I"]["vmax"].value for f in self.instrument.dets.keys()])
+            transform = MinMaxScaler(vmins=vmins, vmaxs=vmaxs)
+
         dataset = TrainCMBMap2PatchDataset(
             n_sims = template_split.n_sims,
             freqs = self.instrument.dets.keys(),
             map_fields=self.map_fields,
             label_path_template=cmb_path_template,
             label_handler=self.in_cmb_asset.handler,
-            # label_handler=HealpyMap(),
             feature_path_template=obs_path_template,
             feature_handler=self.in_obs_assets.handler,
             which_patch_dict=which_patch_dict,
-            nside_obs=self.nside,
-            nside_patches=self.nside_patch
-            # feature_handler=HealpyMap()
+            lut=self.in_lut_asset.read(),
+            transform=transform
             )
         return dataset
 
