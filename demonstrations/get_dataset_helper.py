7f9c6e1d4e7cd6b6f7b2e1c618289fe37085c668
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2025-01-20 15:08:20 -0600

    tutorial D rewritten

--- demonstrations/get_dataset_helper.py
+++ demonstrations/get_dataset_helper.py
@@ -16,7 +16,10 @@ def get_simulation(cfg, dataset_json_fn, split, sim_num):
     all_shared_links = config_reader.read(json_path)
     key = f"{split}_sim{sim_num:04d}"
     shared_link = all_shared_links[key]
-
+    
+    # TODO: Using Namer twice is clunky, come up with better solution.
+    # We set up a Namer to get the root directory of the dataset
+    #   because the tar includes directory structure
     namer = Namer(cfg)
     path_template = "{root}/{dataset}"
     context_params = dict(
@@ -27,5 +30,17 @@ def get_simulation(cfg, dataset_json_fn, split, sim_num):
 
     with tempfile.TemporaryDirectory() as temp_tar_dir:
         download_shared_link_info(shared_link, temp_tar_dir, dest)
+
+    # This second Namer will give the path to the particular simulation
+    namer = Namer(cfg)
+    path_template = cfg.file_system.default_dataset_template_str
+    context_params = dict(
+        dataset=cfg.dataset_name,
+        stage="Simulation",
+        split=split,
+        sim_num=sim_num
+    )
+    with namer.set_contexts(context_params):
+        dest = namer.path(path_template)
 
     return dest
