9328f117ee9a0f3d61939dfff33b2ed91552f3e4
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2025-01-27 15:11:51 -0600

    allow no_download and just get path

--- get_data/utils/get_planck_data_ext.py
+++ get_data/utils/get_planck_data_ext.py
@@ -16,7 +16,7 @@ def format_real(real):
     return "{:.0f}".format(real).zfill(5)
 
 
-def get_planck_obs_data_ext(detector, assets_directory, progress=False):
+def get_planck_obs_data_ext(detector, assets_directory, progress=False, download=True):
     planck_obs_fn = "{instrument}_SkyMap_{frequency}_{obs_nside}_{rev}_full.fits"
     url_template_maps = "http://pla.esac.esa.int/pla/aio/product-action?MAP.MAP_ID={fn}"
     # Setup to get maps... this is all naming convention stuff
@@ -35,6 +35,9 @@ def get_planck_obs_data_ext(detector, assets_directory, progress=False):
 
     obs_map_fn = planck_obs_fn.format(instrument=instrument, frequency=use_freq_str, rev=rev, obs_nside=obs_nside)
     dest_path = Path(assets_directory) / obs_map_fn
+    if not download:
+        return dest_path
+
     if progress:
         if detector in [30, 44, 70]:
             file_size = 502       # IQU maps at nside=1024 are 503 MB
@@ -42,7 +45,6 @@ def get_planck_obs_data_ext(detector, assets_directory, progress=False):
             file_size = 603       # I maps at nside=2048 are 604 MB
         else:                     # 100, 143, 217, 353
             file_size = 2 * 1000  # IQU maps at nside=2048 are 2 GB
-
         download_progress(dest_path, url_template_maps, file_size=file_size)
     else:
         download(dest_path, url_template_maps)
@@ -50,7 +52,7 @@ def get_planck_obs_data_ext(detector, assets_directory, progress=False):
     return dest_path
 
 
-def get_planck_hm_data_ext(detector, assets_directory, progress=False):
+def get_planck_hm_data_ext(detector, assets_directory, progress=False, download=True):
     hm_map_fn_template = "{instrument}_SkyMap_{freq}_2048_R3.01_halfmission-{hm}.fits"
     url_template_maps = "http://pla.esac.esa.int/pla/aio/product-action?MAP.MAP_ID={fn}"
 
@@ -63,6 +65,9 @@ def get_planck_hm_data_ext(detector, assets_directory, progress=False):
 
     hm_1_fn = Path(assets_directory) / hm_1_fn
     hm_2_fn = Path(assets_directory) / hm_2_fn
+
+    if not download:
+        return hm_1_fn, hm_2_fn
 
     if progress:
         if detector in [30, 44, 70]:
@@ -89,7 +94,7 @@ def get_planck_noise_fn(detector, realization):
     return fn
 
 
-def get_planck_noise_data_ext(detector, assets_directory, realization=0, progress=False):
+def get_planck_noise_data_ext(detector, assets_directory, realization=0, progress=False, download=True):
     """
     Get the filepath for the Planck noise data, downloading it if necessary.
 
@@ -108,6 +113,10 @@ def get_planck_noise_data_ext(detector, assets_directory, realization=0, progres
 
     fn = Path(assets_directory) / get_planck_noise_fn(detector, realization)
     url_template_sims = "http://pla.esac.esa.int/pla/aio/product-action?SIMULATED_MAP.FILE_ID={fn}"
+
+    if not download:
+        return fn
+
     if progress:
         download_progress(fn, url_template_sims, file_size=file_size)
     else:
@@ -115,7 +124,7 @@ def get_planck_noise_data_ext(detector, assets_directory, realization=0, progres
     return fn
 
 
-def get_planck_pred_data_ext(assets_directory, fn=None, file_size=None, progress=False):
+def get_planck_pred_data_ext(assets_directory, fn=None, file_size=None, progress=False, download=True):
     """
     Get the filepath for the Planck's prediction map, downloading it if necessary.
 
@@ -139,6 +148,10 @@ def get_planck_pred_data_ext(assets_directory, fn=None, file_size=None, progress
     url_template = "http://pla.esac.esa.int/pla/aio/product-action?MAP.MAP_ID={fn}"
 
     dest_fp = Path(assets_directory) / fn
+
+    if not download:
+        return dest_fp
+
     if progress:
         download_progress(dest_fp, url_template, file_size=file_size)
     else:
