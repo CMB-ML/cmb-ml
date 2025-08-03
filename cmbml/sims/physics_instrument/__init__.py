e7713af6a4431ba6d03337fb2db6e577eea795c4
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2025-08-03 17:11:23 -0500

    added corr aniso noise with only non-stationary noise (disabled)

--- cmbml/sims/physics_instrument/__init__.py
+++ cmbml/sims/physics_instrument/__init__.py
@@ -1,6 +1,7 @@
 from .physics_instrument_noise_empty import EmptyNoise
 from .physics_instrument_noise_variance import VarianceNoise
 from .physics_instrument_noise_spatial_corr import SpatialCorrNoise
+from .physics_instrument_noise_corr_aniso_nonstat import CorrAnisoNoise
 
 
 def get_noise_class(label):
@@ -10,5 +11,7 @@ def get_noise_class(label):
         return VarianceNoise
     elif label == 'spatial_corr':
         return SpatialCorrNoise
+    elif label == 'corr_aniso_nonstat':
+        return CorrAnisoNoise
     else:
         raise ValueError(f"Unsupported noise type: {label}")
