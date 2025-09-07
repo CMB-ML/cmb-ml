4716f09866cfc9f2edf5c49d955e438da4269248
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2025-09-07 13:12:04 -0500

    improved error message

--- cmbml/core/asset_handlers/asset_handler_registration.py
+++ cmbml/core/asset_handlers/asset_handler_registration.py
@@ -5,10 +5,13 @@ def register_handler(handler_name, handler_class):
     _handlers[handler_name] = handler_class
 
 
-def get_handler(asset_info):
+def get_handler(asset_info, source_stage=None):
     handler_name = asset_info.get("handler")
     try:
         handler_class = _handlers[handler_name]
     except KeyError:
-        raise KeyError(f"Handler {handler_name} not available. Ensure it's correct.")
+        src_str = ""
+        if source_stage is not None:
+            src_str = f" within {source_stage}"
+        raise KeyError(f"Handler {handler_name} not available{src_str}. Ensure it's correct and properly registered.")
     return handler_class
