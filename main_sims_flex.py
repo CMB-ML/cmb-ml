d2dab081e9dc883b36cf64b45c937f54136f6296
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2026-01-05 22:47:12 -0600

    no-opped files

--- main_sims_flex.py
+++ main_sims_flex.py
@@ -1,98 +1,98 @@
-"""
-This script runs a simulation pipeline for generating simulations based on a configuration.
+# """
+# This script runs a simulation pipeline for generating simulations based on a configuration.
 
-The pipeline consists of several steps, including checking configurations, 
-creating needed precursor assets (noise cache, configs, and theory power spectra), 
-and creating the simulations.
+# The pipeline consists of several steps, including checking configurations, 
+# creating needed precursor assets (noise cache, configs, and theory power spectra), 
+# and creating the simulations.
 
-The script uses the Hydra library for configuration management.
+# The script uses the Hydra library for configuration management.
 
-To run the simulation pipeline, execute the `run_simulations()` function.
+# To run the simulation pipeline, execute the `run_simulations()` function.
 
-Example usage:
-    python main_sims.py
-"""
+# Example usage:
+#     python main_sims.py
+# """
 
-import logging
-import hydra
-from omegaconf import OmegaConf
-from cmbml.core import PipelineContext, LogMaker
-from cmbml.core.A_check_hydra_configs import HydraConfigCheckerExecutor
-from cmbml.sims import (
-    HydraConfigSimsCheckerExecutor,
-    NoiseCacheExecutor,
-    FixedForegroundExecutor,
-    GetPlanckNoiseSimsExecutor,
-    MakePlanckAverageNoiseExecutor,
-    MakePlanckNoiseModelExecutor,
-    DownloadNoiseModelExecutor,
-    ChainsConfigExecutor,
-    ParamConfigExecutor,
-    ForegroundConfigExecutor,
-    TheoryPSExecutor,
-    ObsCreatorExecutor,
-    # VariedFGsExecutor,
-    FlexObsCreatorExecutor,
-    NoiseMapCreatorExecutor,
-    SimCreatorExecutor,
-    NoiseFilterSimCreatorExecutor,
-    SingleNoiseSimCreatorExecutor
-)
+# import logging
+# import hydra
+# from omegaconf import OmegaConf
+# from cmbml.core import PipelineContext, LogMaker
+# from cmbml.core.A_check_hydra_configs import HydraConfigCheckerExecutor
+# from cmbml.sims import (
+#     HydraConfigSimsCheckerExecutor,
+#     NoiseCacheExecutor,
+#     PySMForegroundPrepExecutor,
+#     GetPlanckNoiseSimsExecutor,
+#     MakePlanckAverageNoiseExecutor,
+#     MakePlanckNoiseModelExecutor,
+#     DownloadNoiseModelExecutor,
+#     ChainsConfigExecutor,
+#     ParamConfigExecutor,
+#     ForegroundConfigExecutor,
+#     TheoryPSExecutor,
+#     ObsCreatorExecutor,
+#     # VariedFGsExecutor,
+#     FlexObsCreatorExecutor,
+#     NoiseMapCreatorExecutor,
+#     SimCreatorExecutor,
+#     NoiseFilterSimCreatorExecutor,
+#     SingleNoiseSimCreatorExecutor
+# )
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
 
-@hydra.main(version_base=None, config_name="config_sim_flex")
-def run_simulations(cfg):
-    """
-    Runs the simulation pipeline.
+# @hydra.main(version_base=None, config_name="config_sim_flex")
+# def run_simulations(cfg):
+#     """
+#     Runs the simulation pipeline.
 
-    Args:
-        cfg: The configuration object.
+#     Args:
+#         cfg: The configuration object.
 
-    Raises:
-        Exception: If an exception occurs during the pipeline execution.
-    """
-    logger.debug(f"Running {__name__} in {__file__}")
+#     Raises:
+#         Exception: If an exception occurs during the pipeline execution.
+#     """
+#     logger.debug(f"Running {__name__} in {__file__}")
 
-    print(OmegaConf.to_yaml(cfg))
+#     print(OmegaConf.to_yaml(cfg))
 
-    log_maker = LogMaker(cfg)
-    log_maker.log_procedure_to_hydra(source_script=__file__)
+#     log_maker = LogMaker(cfg)
+#     log_maker.log_procedure_to_hydra(source_script=__file__)
 
-    pipeline_context = PipelineContext(cfg, log_maker)
+#     pipeline_context = PipelineContext(cfg, log_maker)
 
-    pipeline_context.add_pipe(DownloadNoiseModelExecutor)
-    pipeline_context.add_pipe(NoiseCacheExecutor)
-    pipeline_context.add_pipe(FixedForegroundExecutor)
-    if cfg.model.sim.cmb.use_chains:
-        pipeline_context.add_pipe(ChainsConfigExecutor)
-    else:
-        pipeline_context.add_pipe(ParamConfigExecutor)
-    pipeline_context.add_pipe(TheoryPSExecutor)
-    pipeline_context.add_pipe(ForegroundConfigExecutor)
-    pipeline_context.add_pipe(FlexObsCreatorExecutor)
-    pipeline_context.add_pipe(NoiseMapCreatorExecutor)
-    pipeline_context.add_pipe(SingleNoiseSimCreatorExecutor)
+#     # pipeline_context.add_pipe(DownloadNoiseModelExecutor)
+#     # pipeline_context.add_pipe(NoiseCacheExecutor)
+#     # pipeline_context.add_pipe(FixedForegroundExecutor)
+#     # if cfg.model.sim.cmb.use_chains:
+#     #     pipeline_context.add_pipe(ChainsConfigExecutor)
+#     # else:
+#     #     pipeline_context.add_pipe(ParamConfigExecutor)
+#     # pipeline_context.add_pipe(TheoryPSExecutor)
+#     pipeline_context.add_pipe(ForegroundConfigExecutor)
+#     # pipeline_context.add_pipe(FlexObsCreatorExecutor)
+#     pipeline_context.add_pipe(NoiseMapCreatorExecutor)
+#     # pipeline_context.add_pipe(SingleNoiseSimCreatorExecutor)
 
-    # # TODO: Put this back in the pipeline yaml; fix/make executor
-    # # pipeline_context.add_pipe(ShowSimsExecutor)  # Out of date, do not use.
+#     # # TODO: Put this back in the pipeline yaml; fix/make executor
+#     # # pipeline_context.add_pipe(ShowSimsExecutor)  # Out of date, do not use.
 
-    pipeline_context.prerun_pipeline()
+#     pipeline_context.prerun_pipeline()
 
-    had_exception = False
-    try:
-        pipeline_context.run_pipeline()
-    except Exception as e:
-        had_exception = True
-        logger.exception("An exception occurred during the pipeline.", exc_info=e)
-        raise e
-    finally:
-        if had_exception:
-            logger.error("Pipeline failed.")
-        else:
-            logger.info("Pipeline completed.")
-        log_maker.copy_hydra_run_to_dataset_log()
+#     had_exception = False
+#     try:
+#         pipeline_context.run_pipeline()
+#     except Exception as e:
+#         had_exception = True
+#         logger.exception("An exception occurred during the pipeline.", exc_info=e)
+#         raise e
+#     finally:
+#         if had_exception:
+#             logger.error("Pipeline failed.")
+#         else:
+#             logger.info("Pipeline completed.")
+#         log_maker.copy_hydra_run_to_dataset_log()
 
-if __name__ == "__main__":
-    run_simulations()
+# if __name__ == "__main__":
+#     run_simulations()
