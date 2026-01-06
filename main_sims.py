da162a6be07eed6bcb243b6d2e7e8bdce71bf429
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2026-01-05 22:50:26 -0600

    prep for run

--- main_sims.py
+++ main_sims.py
@@ -28,8 +28,9 @@ from cmbml.sims import (
     ChainsConfigExecutor,
     ParamConfigExecutor,
     TheoryPSExecutor,
-    ObsCreatorExecutor,
-    PrepForegroundsExecutor,
+    ForegroundConfigExecutor,
+    PySMForegroundPrepExecutor,
+    FlexObsCreatorExecutor,
     NoiseMapCreatorExecutor,
     SimCreatorExecutor,
     NoiseFilterSimCreatorExecutor
@@ -38,7 +39,7 @@ from cmbml.sims import (
 logger = logging.getLogger(__name__)
 
 
-@hydra.main(version_base=None, config_name="config_sim")
+@hydra.main(version_base=None, config_name="config_sim_flex")
 def run_simulations(cfg):
     """
     Runs the simulation pipeline.
@@ -58,51 +59,49 @@ def run_simulations(cfg):
 
     pipeline_context = PipelineContext(cfg, log_maker)
 
-    # # pipeline_context.add_pipe(HydraConfigCheckerExecutor)
-    # # pipeline_context.add_pipe(HydraConfigSimsCheckerExecutor)
+    # Required for the kinds of noise implemented in the pipeline
+    pipeline_context.add_pipe(NoiseCacheExecutor)
 
-    # # Required for the kinds of noise implemented in the pipeline
-    # pipeline_context.add_pipe(NoiseCacheExecutor)
+    ############################
+    # Noise model creation
+    ############################
+    # If using spatially correlated noise, either recreate or download the noise model
+    # Recreation requires GetPlanckNoiseSimsExecutor, 
+    #                     MakePlanckAverageNoiseExecutor, 
+    #                 and MakePlanckNoiseModelExecutor
+    # Downloading requires DownloadNoiseModelExecutor only
 
-    # ############################
-    # # Noise model creation
-    # ############################
-    # # If using spatially correlated noise, either recreate or download the noise model
-    # # Recreation requires GetPlanckNoiseSimsExecutor, MakePlanckAverageNoiseExecutor, and MakePlanckNoiseModelExecutor
-    # # Downloading requires DownloadNoiseModelExecutor only
+    # Recreate the noise model (slow)
+    # Download the number of noise sims defined in noise_spatial_corr.yaml (if not present already)
+    # pipeline_context.add_pipe(GetPlanckNoiseSimsExecutor)  # Full resolution! Lots of maps! NERST likes to fail when downloading this many!
+    # Average the noise sims (slow); produces a single noise map per frequency
+    # pipeline_context.add_pipe(MakePlanckAverageNoiseExecutor)
+    # Create the noise model, requiring SHT of each map (slow)
+    # pipeline_context.add_pipe(MakePlanckNoiseModelExecutor)
 
-    # # Recreate the noise model (slow)
-    # # Download the number of noise sims defined in noise_spatial_corr.yaml (if not present already)
-    # # pipeline_context.add_pipe(GetPlanckNoiseSimsExecutor)  # Full resolution! Lots of maps!
-    # # Average the noise sims (slow); produces a single noise map per frequency
-    # # pipeline_context.add_pipe(MakePlanckAverageNoiseExecutor)
-    # # Create the noise model, requiring SHT of each map (slow)
-    # # pipeline_context.add_pipe(MakePlanckNoiseModelExecutor)
-
-    # # Download the noise model (much faster)
-    # # The noise model is a summary of 100 Planck noise maps in the form of
-    # # an average noise map, a power spectrum, and a noise covariance matrix
-    # # for each detector frequency. It's much smaller than the original data
-    # # (processed above in commented out Executors)
-    # pipeline_context.add_pipe(DownloadNoiseModelExecutor)
+    # Download the noise model (much faster)
+    # The noise model is a summary of 100 Planck noise maps in the form of
+    # an average noise map, a power spectrum, and a noise covariance matrix
+    # for each detector frequency. It's much smaller than the original data
+    # (processed above in commented out Executors)
+    pipeline_context.add_pipe(DownloadNoiseModelExecutor)
 
     ############################
     # Simulation creation
     ############################
 
-    # # Needed for all:
-    # if cfg.model.sim.cmb.use_chains:
-    #     pipeline_context.add_pipe(ChainsConfigExecutor)
-    # else:
-    #     pipeline_context.add_pipe(ParamConfigExecutor)
-    # pipeline_context.add_pipe(TheoryPSExecutor)
-    # pipeline_context.add_pipe(ObsCreatorExecutor)
-    # pipeline_context.add_pipe(NoiseMapCreatorExecutor)
-    # pipeline_context.add_pipe(SimCreatorExecutor)
-    pipeline_context.add_pipe(NoiseFilterSimCreatorExecutor)
+    # Needed for all:
 
-    # # TODO: Put this back in the pipeline yaml; fix/make executor
-    # # pipeline_context.add_pipe(ShowSimsExecutor)  # Out of date, do not use.
+    if cfg.model.sim.cmb.use_chains:
+        pipeline_context.add_pipe(ChainsConfigExecutor)
+    else:
+        pipeline_context.add_pipe(ParamConfigExecutor)
+    pipeline_context.add_pipe(TheoryPSExecutor)
+    pipeline_context.add_pipe(ForegroundConfigExecutor)
+    pipeline_context.add_pipe(PySMForegroundPrepExecutor)
+    pipeline_context.add_pipe(FlexObsCreatorExecutor)
+    pipeline_context.add_pipe(NoiseMapCreatorExecutor)
+    pipeline_context.add_pipe(SimCreatorExecutor)
 
     pipeline_context.prerun_pipeline()
 
