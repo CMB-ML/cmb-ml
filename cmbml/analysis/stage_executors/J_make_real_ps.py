d4ed9b0b06fbb0a8bb595c00b28e3dd9d921db95
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2026-01-19 20:14:33 -0600

    log entry for mask file source now notes if using sm mask

--- cmbml/analysis/stage_executors/J_make_real_ps.py
+++ cmbml/analysis/stage_executors/J_make_real_ps.py
@@ -63,10 +63,11 @@ class MakeRealPowerSpectrumExecutor(BaseStageExecutor):
     def get_masks(self):
         mask = None
         with self.name_tracker.set_context("src_root", self.cfg.local_system.assets_dir):
-            logger.info(f"Using mask from {self.in_mask.path}")
             if self.use_sm_mask:
+                logger.info(f"Using mask from {self.in_mask_sm.path}")
                 mask = self.in_mask_sm.read(map_fields=self.in_mask_sm.use_fields)[0]
             else:
+                logger.info(f"Using mask from {self.in_mask.path}")
                 mask = self.in_mask.read(map_fields=self.in_mask.use_fields)[0]
         if hp.npix2nside(mask.size) != self.nside_out:
             mask = downgrade_mask(mask, self.nside_out, threshold=self.mask_threshold)
