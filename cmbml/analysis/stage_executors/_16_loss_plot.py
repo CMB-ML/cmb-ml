616f82410d317cc38058c01e22140026a26a91b4
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2026-02-26 19:48:08 -0600

    updated loss plot for use externally

--- cmbml/analysis/stage_executors/_16_loss_plot.py
+++ cmbml/analysis/stage_executors/_16_loss_plot.py
@@ -24,7 +24,7 @@ class LossPlotExecutor(BaseStageExecutor):
                                  fig_label=self.fig_label)
         self.out_fig.write(fig=fig)
 
-def make_loss_plot(some_path, fig_label, figsize=(10,6)):
+def make_loss_plot(some_path, fig_label, figsize=(10,6), fig=None, ax=None, ylim=None, sm_sigma2=None):
         df = pd.read_csv(some_path)
         #skipping the first few rows if they are not needed
         # df = df[df["Epoch"] >= self.skip_n_values]
@@ -33,7 +33,11 @@ def make_loss_plot(some_path, fig_label, figsize=(10,6)):
         best_val_loss = df.loc[best_idx, "Validation Loss"]
         best_epoch = df.loc[best_idx, "Epoch"]
 
-        fig, ax = plt.subplots(figsize=figsize)
+        if (fig is None and ax is not None) or (fig is not None and ax is None):
+             raise ValueError("fig and ax must be provided together")
+
+        if ax is None:
+            fig, ax = plt.subplots(figsize=figsize)
         ax.plot(df["Epoch"], df["Training Loss"], label="Training Loss", color="tab:blue", alpha=0.6, linewidth=0.5)
         ax.plot(df["Epoch"], df["Validation Loss"], label="Validation Loss", color="tab:orange", alpha=0.8, linewidth=0.5)
 
@@ -41,7 +45,7 @@ def make_loss_plot(some_path, fig_label, figsize=(10,6)):
         ax.annotate(
             f"Best validation loss\n${loss_txt}$ at epoch {best_epoch}",
             xy=(best_epoch, best_val_loss),
-            xytext=(0, best_val_loss),
+            xytext=(50, 1e6),
             arrowprops=dict(arrowstyle="->", color="black"),
             fontsize=10,
             bbox=dict(boxstyle="round", facecolor="white", alpha=0.8)
@@ -56,14 +60,50 @@ def make_loss_plot(some_path, fig_label, figsize=(10,6)):
         train_smoothed = gaussian_filter1d(train_log, sigma=5)
         val_smoothed   = gaussian_filter1d(val_log, sigma=5)
 
+        train_end_loss_txt = exp_as_ltx(np.exp(train_smoothed[-1]))
+        valid_end_loss_txt = exp_as_ltx(np.exp(val_smoothed[-1]))
+        
         ax.plot(df["Epoch"], np.exp(train_smoothed), label="Smoothed Training Loss", linestyle="--", linewidth=2, color="tab:blue")
         ax.plot(df["Epoch"], np.exp(val_smoothed), label="Smoothed Validation Loss", linestyle="--", linewidth=2, color="tab:orange")
+
+        if sm_sigma2 is not None:
+            train_smoothed = gaussian_filter1d(train_log, sigma=sm_sigma2)
+            val_smoothed   = gaussian_filter1d(val_log, sigma=sm_sigma2)
+
+            ax.plot(df["Epoch"], np.exp(train_smoothed), 
+                    label="Smoothed Training Loss", 
+                    linestyle="--", linewidth=4, alpha=0.5, 
+                    color="tab:blue")
+            ax.plot(df["Epoch"], np.exp(val_smoothed), 
+                    label="Smoothed Validation Loss", 
+                    linestyle="--", linewidth=4, alpha=0.5, 
+                    color="tab:orange")
+
+            ax.annotate(
+                f"Approx Train loss\n${train_end_loss_txt}$",
+                xy=(df["Epoch"].iloc[-1], np.exp(train_smoothed[-1])),
+                xytext=(150, 5e5),
+                arrowprops=dict(arrowstyle="->", color="black"),
+                fontsize=10,
+                bbox=dict(boxstyle="round", facecolor="white", alpha=0.8)
+            )
+            ax.annotate(
+                f"Approx Valid loss\n${valid_end_loss_txt}$",
+                xy=(df["Epoch"].iloc[-1], np.exp(val_smoothed[-1])),
+                xytext=(200, 1.5e6),
+                arrowprops=dict(arrowstyle="->", color="black"),
+                fontsize=10,
+                bbox=dict(boxstyle="round", facecolor="white", alpha=0.8)
+            )
 
         ax.set_yscale('log')
 
         ax.set_xlabel("Epoch")
-        ax.set_ylabel("Log Loss")
+        ax.set_ylabel("Loss")
         ax.set_title(f"Training vs Validation Loss, {fig_label}")
+
+        if ylim is not None:
+            ax.set_ylim(*ylim)
 
         custom_legend = [
             Line2D([0], [0], color="tab:blue", linestyle="-", linewidth=2, label="Training Loss"),
