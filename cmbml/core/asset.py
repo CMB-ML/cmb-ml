faf637373490e7050bb0183109b785b7c89f4fe3
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2026-05-10 16:51:07 -0500

    rewrite for clarity

--- cmbml/core/asset.py
+++ cmbml/core/asset.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import logging
+from typing import Optional
 
-from omegaconf import DictConfig
 from omegaconf import errors as OmegaErrors
 
 from .namers import Namer
@@ -14,137 +14,139 @@ logger = logging.getLogger(__name__)
 
 
 class Asset:
+    supports_alt_path = False
+
     def __init__(self, cfg, source_stage, asset_name, name_tracker, in_or_out):
         stage_cfg = cfg.pipeline[source_stage]
         asset_info = stage_cfg.assets_out[asset_name]
 
-        self.source_stage_dir = stage_cfg.get('dir_name', None)
-        # self.fn = asset_info.get('fn', "")
+        self.source_stage_dir = stage_cfg.get("dir_name", None)
+        self.name_tracker: Namer = name_tracker
 
-        self.name_tracker:Namer = name_tracker
-        self.can_read = False
-        self.can_write = False
-        if in_or_out == "in":
-            self.can_read = True
-        if in_or_out == "out":
-            self.can_write = True
+        self.can_read = in_or_out == "in"
+        self.can_write = in_or_out == "out"
 
         handler: GenericHandler = get_handler(asset_info, source_stage)
         self.handler = handler()
 
-        try:
-            self.path_template = asset_info.get('path_template', None)
-        except OmegaErrors.InterpolationKeyError as e:
-            # This is a workaround. 
-            # The path template for WMAP9 chains uses interpolation. We set a special sentinel value
-            #   to indicate that the path template is not available. This allows us to work with other
-            #   assets that do not use interpolation.
-            self.path_template = FailedPathInterpolationSentinel(
-                asset_name=asset_name,
-                source_stage=source_stage,
-                error=e,
-            )
-
-        if self.path_template is None:
-            logger.warning("No template found.")
-            # TODO: Remove? Think through this?
-            raise Exception("No path template found! No known good reasons for this dead end...")
+        self.path_template = self._get_path_template(
+            asset_info=asset_info,
+            asset_name=asset_name,
+            source_stage=source_stage,
+        )
 
         self.use_fields = asset_info.get("use_fields", None)
         self.file_size = asset_info.get("file_size", None)
         self.path_overrides = {}
 
-    @property
-    def path(self):
+    def _get_path_template(self, *, asset_info, asset_name, source_stage):
+        try:
+            path_template = asset_info.get("path_template", None)
+        except OmegaErrors.InterpolationKeyError as e:
+            return FailedPathInterpolationSentinel(
+                asset_name=asset_name,
+                source_stage=source_stage,
+                error=e,
+            )
+
+        if path_template is None:
+            raise ValueError(
+                f"No path_template found for asset {asset_name!r} "
+                f"from source stage {source_stage!r}."
+            )
+
+        return path_template
+
+    def _path_from_template(self, path_template):
         with self.name_tracker.set_context("stage", self.source_stage_dir):
             if self.path_overrides:
-                # If there are path overrides, use them
                 with self.name_tracker.set_contexts(self.path_overrides):
-                    return self.name_tracker.path(self.path_template)
-            else:
-                return self.name_tracker.path(self.path_template)
+                    return self.name_tracker.path(path_template)
+            return self.name_tracker.path(path_template)
 
-    def read(self, *, use_alt_path: bool = False, **kwargs):
+    @property
+    def path(self):
+        return self._path_from_template(self.path_template)
+
+    def resolve_path(self, *, use_alt_path: Optional[bool] = None, for_write: bool = False):
         if use_alt_path:
             raise ValueError(f"{type(self).__name__} does not support alternate paths.")
+
+        return self.path
+
+    def read(self, *args, use_alt_path: Optional[bool] = None, **kwargs):
+        if args:
+            raise TypeError(
+                f"{type(self).__name__}.read() only accepts keyword arguments. "
+                f"Received positional args: {args!r}"
+            )
+
         if not self.can_read:
             raise AttributeError("This asset is not set up to read.")
-        return self.handler.read(self.path, **kwargs)
 
-    def start(self, **kwargs):
+        path = self.resolve_path(use_alt_path=use_alt_path, for_write=False)
+        return self.handler.read(path, **kwargs)
+
+    def start(self, *args, use_alt_path: Optional[bool] = None, **kwargs):
+        if args:
+            raise TypeError(
+                f"{type(self).__name__}.start() only accepts keyword arguments. "
+                f"Received positional args: {args!r}"
+            )
+
         if not self.can_write:
             raise AttributeError("This asset is not set up to write.")
-        try:
-            if self.can_write:
-                return self.handler.start(self.path, **kwargs)
-        except TypeError as e:
-            logger.exception("The calling .write() method must be given keyword arguments only.", exc_info=e)
-            raise e
 
-    def write(self, **kwargs):
+        path = self.resolve_path(use_alt_path=use_alt_path, for_write=True)
+        return self.handler.start(path, **kwargs)
+
+    def write(self, *args, use_alt_path: Optional[bool] = None, **kwargs):
+        if args:
+            raise TypeError(
+                f"{type(self).__name__}.write() only accepts keyword arguments. "
+                f"Received positional args: {args!r}"
+            )
+
         if not self.can_write:
             raise AttributeError("This asset is not set up to write.")
-        # If you're here debugging a "TypeError: write() takes 1 positional argument but 2 were given" error, 
-        #   it's because you're calling .write() with positional arguments. 
-        # It must be called with keyword arguments only.
-        try:
-            if self.can_write:
-                return self.handler.write(self.path, **kwargs)
-        except TypeError as e:
-            logger.exception("The calling .write() method must be given keyword arguments only.", exc_info=e)
-            raise e
+
+        path = self.resolve_path(use_alt_path=use_alt_path, for_write=True)
+        return self.handler.write(path, **kwargs)
 
     def append(self, *args, **kwargs):
-        """
-        Assumes .write(some_path) has been called and the file is set up by the asset handler.
-        """
-        if self.handler.append is None:
-            raise AttributeError("The handler for this asset does not have an append method.")
-        if self.can_write:
-            if args and kwargs:
-                return self.handler.append(*args, **kwargs)
-            elif args:
-                return self.handler.append(*args)
-            elif kwargs:
-                return self.handler.append(**kwargs)
-            else:
-                return self.handler.append()
-        else:
+        if not self.can_write:
             raise AttributeError("This asset is not set up to write.")
 
+        if self.handler.append is None:
+            raise AttributeError("The handler for this asset does not have an append method.")
+
+        return self.handler.append(*args, **kwargs)
+
+
 class AssetWithPathAlts(Asset):
+    supports_alt_path = True
+
     def __init__(self, cfg, source_stage, asset_name, name_tracker, in_or_out):
         super().__init__(cfg, source_stage, asset_name, name_tracker, in_or_out)
+
         stage_cfg = cfg.pipeline[source_stage]
         asset_info = stage_cfg.assets_out[asset_name]
+
         self.path_template_alt = asset_info.path_template_alt
 
     @property
     def path_alt(self):
-        with self.name_tracker.set_context("stage", self.source_stage_dir):
-            if self.path_overrides:
-                # If there are path overrides, use them
-                with self.name_tracker.set_contexts(self.path_overrides):
-                    return self.name_tracker.path(self.path_template_alt)
-            else:
-                return self.name_tracker.path(self.path_template_alt)
+        return self._path_from_template(self.path_template_alt)
 
-    def read(self, *, use_alt_path:bool=None, **kwargs):
+    def resolve_path(self, *, use_alt_path: Optional[bool] = None, for_write: bool = False):
         if use_alt_path is None:
-            raise ValueError("Use alt path must be specified.")
-        if self.can_read:
-            if use_alt_path:
-                return self.handler.read(self.path_alt, **kwargs)
-            else:
-                return self.handler.read(self.path, **kwargs)
-        else:
-            raise AttributeError("This asset is not set up to read.")
+            if for_write:
+                raise ValueError(
+                    f"{type(self).__name__}.write()/start() must specify "
+                    "use_alt_path=True or use_alt_path=False."
+                )
 
-    def write(self, use_alt_path:bool=False, **kwargs):
-        if self.can_write:
-            if use_alt_path:
-                return self.handler.write(self.path_alt, **kwargs)
-            else:
-                return self.handler.write(self.path, **kwargs)
-        else:   
-            raise AttributeError("This asset is not set up to write.")
+            # For reads, default to the alternate/shared path.
+            use_alt_path = True
+
+        return self.path_alt if use_alt_path else self.path
