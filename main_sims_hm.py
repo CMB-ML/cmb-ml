23e17ae416658d25aec99e5bb2b6349786b33961
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2026-07-17 18:10:47 -0500

    corrected config

--- main_sims_hm.py
+++ main_sims_hm.py
@@ -28,7 +28,7 @@ from cmbml.sims.ex import (
 logger = logging.getLogger(__name__)
 
 
-@hydra.main(version_base=None, config_name="config_sim_flex")
+@hydra.main(version_base=None, config_name="config_sim")
 def run_simulations(cfg):
     """
     Runs the simulation pipeline.
